38f618d317359f6200bfbc1e393a48e37edda4d1
Daniil <dd@gmail.com>
2025-10-10 13:50:58 +0300

    redacted

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const rawData = {
-    "users_id": "1000,1001",
+    "users_id": "1000,1001,1002,1003,1004,1005,1006,1007,1008,1009",
     "data": [
         {
             "id": 1000,
             "fio": {
-                "name1": "Петя",
-                "family": "Петров",
-                "name2": "Петрович"
+                "name1": "Александр",
+                "family": "Смирнов",
+                "name2": "Сергеевич"
             },
             "schedule": {
-                "10-01": "В",
-                "10-02": "В",
-                "10-03": "У"
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "Д",
+                "10-09": "Д",
+                "10-10": "О",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "Д",
+                "10-15": "Д",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "О",
+                "10-29": "Д",
+                "10-30": "Д",
+                "10-31": "Д"
             }
         },
         {
@@ -19,16 +47,372 @@ const rawData = {
             "fio": {
                 "name1": "Иван",
                 "family": "Иванов",
-                "name2": "Иванович"
+                "name2": "Алексеевич"
             },
             "schedule": {
                 "10-01": "Д",
                 "10-02": "Д",
-                "10-03": "Д"
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "Д",
+                "10-09": "Д",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "Д",
+                "10-15": "О",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "Д",
+                "10-30": "Д",
+                "10-31": "О"
+            }
+        },
+        {
+            "id": 1002,
+            "fio": {
+                "name1": "Мария",
+                "family": "Кузнецова",
+                "name2": "Владимировна"
+            },
+            "schedule": {
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "О",
+                "10-08": "Д",
+                "10-09": "Д",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "Д",
+                "10-15": "Д",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "О",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "Д",
+                "10-30": "Д",
+                "10-31": "Д"
+            }
+        },
+        {
+            "id": 1003,
+            "fio": {
+                "name1": "Екатерина",
+                "family": "Попова",
+                "name2": "Андреевна"
+            },
+            "schedule": {
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "Д",
+                "10-09": "Д",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "О",
+                "10-14": "Д",
+                "10-15": "Д",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "Д",
+                "10-30": "О",
+                "10-31": "Д"
+            }
+        },
+        {
+            "id": 1004,
+            "fio": {
+                "name1": "Дмитрий",
+                "family": "Соколов",
+                "name2": "Михайлович"
+            },
+            "schedule": {
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "О",
+                "10-09": "Д",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "Д",
+                "10-15": "Д",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "О",
+                "10-30": "Д",
+                "10-31": "Д"
+            }
+        },
+        {
+            "id": 1005,
+            "fio": {
+                "name1": "Анна",
+                "family": "Морозова",
+                "name2": "Игоревна"
+            },
+            "schedule": {
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "Д",
+                "10-09": "Д",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "Д",
+                "10-15": "Д",
+                "10-16": "О",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "Д",
+                "10-30": "Д",
+                "10-31": "Д"
+            }
+        },
+        {
+            "id": 1006,
+            "fio": {
+                "name1": "Сергей",
+                "family": "Волков",
+                "name2": "Павлович"
+            },
+            "schedule": {
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "Д",
+                "10-09": "Д",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "Д",
+                "10-15": "Д",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "О",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "Д",
+                "10-30": "Д",
+                "10-31": "О"
+            }
+        },
+        {
+            "id": 1007,
+            "fio": {
+                "name1": "Ольга",
+                "family": "Новикова",
+                "name2": "Дмитриевна"
+            },
+            "schedule": {
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "Д",
+                "10-09": "О",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "Д",
+                "10-15": "Д",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "Д",
+                "10-30": "Д",
+                "10-31": "Д"
+            }
+        },
+        {
+            "id": 1008,
+            "fio": {
+                "name1": "Николай",
+                "family": "Федоров",
+                "name2": "Викторович"
+            },
+            "schedule": {
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "Д",
+                "10-09": "Д",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "О",
+                "10-15": "Д",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "Д",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "Д",
+                "10-30": "Д",
+                "10-31": "Д"
+            }
+        },
+        {
+            "id": 1009,
+            "fio": {
+                "name1": "Елена",
+                "family": "Михайлова",
+                "name2": "Александровна"
+            },
+            "schedule": {
+                "10-01": "Д",
+                "10-02": "Д",
+                "10-03": "Д",
+                "10-04": "В",
+                "10-05": "В",
+                "10-06": "Д",
+                "10-07": "Д",
+                "10-08": "Д",
+                "10-09": "Д",
+                "10-10": "Д",
+                "10-11": "В",
+                "10-12": "В",
+                "10-13": "Д",
+                "10-14": "Д",
+                "10-15": "Д",
+                "10-16": "Д",
+                "10-17": "Д",
+                "10-18": "В",
+                "10-19": "В",
+                "10-20": "Д",
+                "10-21": "О",
+                "10-22": "Д",
+                "10-23": "Д",
+                "10-24": "Д",
+                "10-25": "В",
+                "10-26": "В",
+                "10-27": "Д",
+                "10-28": "Д",
+                "10-29": "Д",
+                "10-30": "Д",
+                "10-31": "Д"
             }
         }
     ]
-};
+}
 
 // Преобразуем данные в удобный формат
 const employeeData = rawData.data.map(employee => ({
@@ -39,29 +423,103 @@ const employeeData = rawData.data.map(employee => ({
     }))
 }));
 
-console.log(employeeData)
+const months = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
 
 // Функция рендеринга таблиц
-function renderTables(data, days = 7) {
+function renderTables(data, period = '3months') {
     const nameBody = document.getElementById('nameBody');
     const scheduleBody = document.getElementById('scheduleBody');
-    const scheduleHeader = document.getElementById('scheduleHeader');
+    const nameThead = document.getElementById('nameThead');
+    const scheduleThead = document.getElementById('scheduleThead');
     nameBody.innerHTML = '';
     scheduleBody.innerHTML = '';
-    scheduleHeader.innerHTML = '';
+    nameThead.innerHTML = '';
+    scheduleThead.innerHTML = '';
 
-    // Генерируем даты для заголовков
-    const dates = Array.from({ length: days }, (_, i) => {
-        const d = new Date(2025, 9, 1 + i); // Начинаем с 2025-10-01
-        return d.toISOString().split('T')[0];
+    const today = new Date(2025, 9, 10); // Текущая дата: 10 октября 2025
+    today.setUTCHours(3, 0, 0, 0); // Московское время (UTC+3)
+
+    let startDate = new Date(today);
+    let endDate = new Date(today);
+
+    if (period === '3months') {
+        const quarter = Math.floor(today.getMonth() / 3);
+        startDate = new Date(today.getFullYear(), quarter * 3, 1);
+        endDate = new Date(today.getFullYear(), (quarter + 1) * 3, 0);
+    } else if (period === '1month') {
+        startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+        endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+    } else if (period === '7days') {
+        const dayOfWeek = today.getDay(); // 0=воскресенье, 1=понедельник, ..., 6=суббота
+        const mondayOffset = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
+        startDate = new Date(today);
+        startDate.setDate(today.getDate() + mondayOffset);
+        endDate = new Date(startDate);
+        endDate.setDate(startDate.getDate() + 6);
+    }
+
+    // Генерируем даты
+    const dates = [];
+    let currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+        dates.push(currentDate.toLocaleDateString('ru-RU', { timeZone: 'Europe/Moscow' }).split('.')[2] + '-' +
+                    (currentDate.getMonth() + 1).toString().padStart(2, '0') + '-' +
+                    currentDate.getDate().toString().padStart(2, '0'));
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    // Группировка по месяцам для colspan
+    const curDate = new Date()
+    let monthGroups = [];
+    let currentMonth = null;
+    let colspan = 0;
+    dates.forEach(dateStr => {
+        const d = new Date(dateStr);
+        const monthIndex = d.getMonth();
+        if (monthIndex !== currentMonth) {
+            if (colspan > 0) {
+                monthGroups.push({ month: months[currentMonth], colspan });
+            }
+            currentMonth = monthIndex;
+            colspan = 1;
+        } else {
+            colspan++;
+        }
     });
+    if (colspan > 0) {
+        monthGroups.push({ month: months[currentMonth], colspan });
+    }
 
-    // Рендерим заголовки дат
-    dates.forEach(date => {
+    // Рендерим thead для фиксированной колонки
+    const emptyMonthTr = document.createElement('tr');
+    const emptyMonthTh = document.createElement('th');
+    emptyMonthTr.appendChild(emptyMonthTh);
+    nameThead.appendChild(emptyMonthTr);
+
+    const nameTr = document.createElement('tr');
+    const nameTh = document.createElement('th');
+    nameTh.textContent = '';
+    nameTr.appendChild(nameTh);
+    nameThead.appendChild(nameTr);
+
+    // Рендерим thead для прокручиваемой таблицы
+    const monthTr = document.createElement('tr');
+    monthGroups.forEach(group => {
         const th = document.createElement('th');
-        th.textContent = date;
-        scheduleHeader.appendChild(th);
+        th.colSpan = group.colspan;
+        th.textContent = group.month;
+        monthTr.appendChild(th);
     });
+    scheduleThead.appendChild(monthTr);
+
+    const dayTr = document.createElement('tr');
+    dates.forEach(dateStr => {
+        const d = new Date(dateStr);
+        const th = document.createElement('th');
+        th.textContent = d.getDate();
+        dayTr.appendChild(th);
+    });
+    scheduleThead.appendChild(dayTr);
 
     // Рендерим строки
     data.forEach(employee => {
@@ -94,18 +552,14 @@ document.getElementById('nameFilter').addEventListener('input', function(e) {
     const filteredData = employeeData.filter(employee =>
         employee.name.toLowerCase().includes(searchValue)
     );
-    const days = parseInt(document.getElementById('periodFilter').value);
-    renderTables(filteredData, days);
+    const period = document.getElementById('periodFilter').value;
+    renderTables(filteredData, period);
 });
 
 // Фильтр по периоду
 document.getElementById('periodFilter').addEventListener('change', function(e) {
-    const days = parseInt(e.target.value);
-    const searchValue = document.getElementById('nameFilter').value.toLowerCase();
-    const filteredData = employeeData.filter(employee =>
-        employee.name.toLowerCase().includes(searchValue)
-    );
-    renderTables(filteredData, days);
+    const period = e.target.value;
+    renderTables(employeeData, period);
 });
 
 // Начальный рендеринг
